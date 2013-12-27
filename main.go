7b246ae3ca1508c53b09dcb080b2ce324e0c0a2b
Eric Levine <eric.levine@airbnb.com>
2013-12-26 17:33:23 -0800

    Gifs are working splendidly.

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
   "strings"
   "image"
+  "image/color"
   "image/png"
+  "image/gif"
   "image/jpeg"
+  "math"
   "os"
   "bufio"
   "code.google.com/p/draw2d/draw2d"
@@ -26,14 +29,68 @@ type TextBounds struct {
 }
 
 func main() {
-  inImage, err := openImage("image.jpeg")
+  //inImage, err := openImage("image.jpeg")
+  //if err != nil { os.Exit(1) }
+
+  //i, err := createMeme(inImage, "hello there my name is eric", "i think that you are really great")
+  //if err != nil { os.Exit(1) }
+
+  //writeImage("out1.png", i)
+  //if err != nil { os.Exit(1) }
+
+  inImage, err := openGif("ian.gif")
   if err != nil { os.Exit(1) }
 
-  i, err := createMeme(inImage, "hello there my name is eric", "i think that you are really great")
+  err = createGifMeme(inImage, "hello there my name is eric", "i think that you are really great")
   if err != nil { os.Exit(1) }
 
-  writeImage("out1.png", i)
+  writeGif("out1.gif", inImage)
   if err != nil { os.Exit(1) }
+}
+
+func createGifMeme(background *gif.GIF, topText, bottomText string) error {
+  firstFrame := background.Image[0]
+
+  overlayMeme, err := createMeme(image.NewRGBA(firstFrame.Bounds()), topText, bottomText)
+  if err != nil { return err }
+
+  for _, pic := range background.Image {
+    injectColor(pic.Palette, image.White)
+    injectColor(pic.Palette, image.Black)
+    bounds := pic.Bounds()
+    for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+      for x := bounds.Min.X; x < bounds.Max.X; x++ {
+        color := overlayMeme.At(x, y)
+        _, _, _, alpha := color.RGBA()
+        if alpha > 0 {
+          pic.Set(x, y, color)
+        }
+      }
+    }
+  }
+
+  return nil
+}
+
+func injectColor(p color.Palette, target color.Color) {
+  if len(p) < 256 {
+    p = append(p, target)
+  } else {
+    tr, tg, tb, ta := target.RGBA()
+    bestScore, bestIndex := math.MaxFloat64, 0
+    for i, candidate := range p {
+      cr, cg, cb, ca := candidate.RGBA()
+      score := math.Pow(float64(tr - cr), 2)
+      score += math.Pow(float64(tg - cg), 2)
+      score += math.Pow(float64(tb - cb), 2)
+      score += math.Pow(float64(ta - ca), 2)
+      if score < bestScore {
+        bestScore = score
+        bestIndex = i
+      }
+    }
+    p[bestIndex] = target
+  }
 }
 
 func createMeme(background image.Image, topText, bottomText string) (image.Image, error) {
@@ -49,7 +106,7 @@ func createMeme(background image.Image, topText, bottomText string) (image.Image
     width: width,
   }
 
-  i := image.NewRGBA(image.Rect(0, 0, int(ctx.width), int(ctx.height)))
+  i := image.NewRGBA(background.Bounds())
   initializeGraphicContext(&ctx, i)
 
   ctx.gc.DrawImage(background)
@@ -70,12 +127,33 @@ func openImage(name string) (image.Image, error) {
   return i, nil
 }
 
+func openGif(name string) (*gif.GIF, error) {
+  f, err := os.Open(name)
+  if err != nil { return nil, err }
+  defer f.Close()
+  i, err := gif.DecodeAll(f)
+  if err != nil { return nil, err }
+  return i, nil
+}
+
 func writeImage(name string, i image.Image) error {
   f, err := os.Create(name)
   if err != nil { return err }
   defer f.Close()
   b := bufio.NewWriter(f)
   err = png.Encode(b, i)
+  if err != nil { return err }
+  err = b.Flush()
+  if err != nil { return err }
+  return nil
+}
+
+func writeGif(name string, i *gif.GIF) error {
+  f, err := os.Create(name)
+  if err != nil { return err }
+  defer f.Close()
+  b := bufio.NewWriter(f)
+  err = gif.EncodeAll(b, i)
   if err != nil { return err }
   err = b.Flush()
   if err != nil { return err }
